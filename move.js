7e8b09efb9d53a56f4b4333d6c2aede7c71566b9
tamccullough88 <tamccullough88@gmail.com>
2023-09-01 21:40:12 -0400

    added movements to character with the functions move to coordinates, move with arrow keys, move character.

--- move.js
+++ move.js
@@ -1,12 +1,70 @@
-function move(element) {
-    element.style.position = 'fixed'
+
+//character move functions, to move character on button press a certain distance, and to stop moving when the button is released//
+
+function move(character) {
+    character.style.position = 'fixed'
+    character.style.zIndex = '5'
 
     function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+        character.style.left = left + 'px'
+        character.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom){
+        let direction = null;
+    let x = left;
+    let y = bottom;
+
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+    
+    function moveCharacter(){ 
+        if(direction === 'west'){
+            x-=1
+        }
+        if(direction === 'north'){
+            y+=1
+        }
+        if(direction === 'east'){
+            x+=1
+        }
+        if(direction === 'south'){
+            y-=1
+        }
+        character.style.left = x + 'px'
+        character.style.bottom = y + 'px'
+    }
+    
+    setInterval(moveCharacter, 1)
+    
+   document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+    handleDirectionChange(direction)
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+    handleDirectionChange(direction)
+})
+
+       
     }
 
     return {
-        to: moveToCoordinates
-    }
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
 }
